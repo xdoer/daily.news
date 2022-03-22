5a1e721667f119acd1d61c2250a78675966b4d12
xdoer <gotoanything@foxmail.com>
2022-03-23 01:19:35 +0800

    feat: 数据整合

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import crawl from '@daily.news/crawl'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return crawl()
   }
 }
